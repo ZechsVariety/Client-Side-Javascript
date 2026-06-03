79c84fba605cd70a7d559c4780cd3b7ddbe93b5f
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-06-03 09:04:46 -0400

    lesson5 break1

--- lesson-5-sphero/1-intro.js
+++ lesson-5-sphero/1-intro.js
@@ -1,12 +1,14 @@
-// STEP 1: Build the main entry point for the program
-
-    // STEP 2: Call a method to control the main LED lights
-    
-    // STEP 3: Invoke another method to write to the screen
-    
-    // STEP 3: Call another method to move the robot
-    
-    // STEP 4: Exit the program
-    
-
-// STEP 5: Aim your robot, then run the program
+// DONE 1: Build the main entry point for the program
+// ? async: functions contains code that js may have to wait for (thus, is asynchronous)
+async function startProgram() {
+    // DONE 2: Call a method to control the main LED lights
+    setMainLed(getRandomColor());
+    // DONE 3: Invoke another method to write to the screen
+    //message, colour, speed fps, blocking (will wait until this block of code is done before continuing. await does the same thing)
+    await scrollMatrixText("Hello World!", getRandomColor(), 24, false);
+    // DONE 3: Call another method to move the robot
+    await roll(0, 30, 2);
+    // DONE 4: Exit the program
+    exitProgram();
+}
+// DONE 5: Aim your robot, then run the program
