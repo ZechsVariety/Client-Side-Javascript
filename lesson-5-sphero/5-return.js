ba7b10cfd01517dbf5bf56923b8eda6d325946f8
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-06-03 08:43:12 -0400

    lesson 5: import sphero js stuff
    also deleted old versions of the code

--- lesson-5-sphero/5-return.js
+++ lesson-5-sphero/5-return.js
@@ -1,21 +1,23 @@
 async function startProgram() {
-	let currentHeading = 0;
-	// Drive the robot in a complete circle, using 12 straight line segments and changing the direction 30 degrees with each iteration of the below loop
-	while (currentHeading < 360) {
-		// STEP 1a: declare a local variable named newLedColor, and initialize it by calling the function getRgbColorFromHue()
-
-		// STEP 1b: pass the currentHeading variable into the function call above as an argument
-		await scrollMatrixText(String(currentHeading), newLedColor, 24, false);
-		await setMainLed(newLedColor);
-		await roll(currentHeading, 50, 1);
-		currentHeading = currentHeading + 30;
-	};
-	await setDisplayImage("slightly-smiling");
+    let currentHeading = 0;
+    // Drive the robot in a complete circle, using 12 straight line segments and changing the direction 30 degrees with each iteration of the below loop
+    while (currentHeading <= 360) {
+        // STEP 1a: declare a local variable named newLedColor, and initialize it by calling the function getRgbColorFromHue()
+        
+        // STEP 1b: pass the currentHeading variable into the function call above as an argument
+        await setMainLed(newLedColor);
+        await scrollMatrixText(currentHeading.toString(), newLedColor, 24, false);
+        await roll(currentHeading, 25, 1);
+        currentHeading = currentHeading + 30;
+    };
+    await setDisplayImage("slightly-smiling");
+    exitProgram();
 };
 
 // Function to convert the heading (0-360 degrees) to a color, based on a color wheel
 // STEP 2: Add a parameter called hueDegrees to the below function
 function getRgbColorFromHue() {
+
     // Calculate raw RGB values using the cosine formula
     let red = 256 * Math.cos(degreesToRadians(hueDegrees));
     let green = 256 * Math.cos(degreesToRadians(hueDegrees + 120));
@@ -32,8 +34,8 @@ function getRgbColorFromHue() {
 
 // Helper function used to convert degrees to radians, as Math.cos expects radians
 function degreesToRadians(degrees) {
-	let radianConversion = degrees * (Math.PI / 180);
-	return radianConversion;
+    let radianConversion = degrees * (Math.PI / 180);
+    return radianConversion;
 };
 
 // STEP 4: Try it out! What customizations can you make to this program?
