31737ddd28f5ef25cb335c8625f70f3bd336c892
ZechsVariety <mortimer@promail.ca>
2026-06-10 12:04:51 -0400

    a1: value selecting for each column

--- assignment-1/js/storymaker.js
+++ assignment-1/js/storymaker.js
@@ -1,5 +1,6 @@
 //objects
 const tableBody = document.querySelector("table tbody");
+const wordButtons = document.querySelector("#wordButtons");
 
 //arrays
 const characters = ["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"];
@@ -33,7 +34,7 @@ function populateColumn(item, index, column) {
     //check if a tr at this index exists, and create one if not
     //thank you techfoobar: https://stackoverflow.com/questions/13107855/how-to-check-if-an-array-index-exists-or-not-in-javascript
     if(typeof rows[index] === "undefined") {
-        console.log(`Row ${index} does not exist! Creating now with 5 empty cells...`);
+        //console.log(`Row ${index} does not exist! Creating now with 5 empty cells...`);
 
         //create new row
         const newRow = document.createElement("tr");
@@ -57,3 +58,40 @@ function populateColumn(item, index, column) {
     cells[column].textContent = item;
     //console.log(cells[column].textContent);
 }
+
+//any of the word buttons are clicked
+wordButtons.addEventListener("click", (event) => {
+    //check that it was infact a button that was clicked
+	if(event.target && event.target.nodeName === "BUTTON") {
+        const targetColumn = event.target.textContent;
+		//console.log(targetColumn);
+
+        //const rows = tableBody.querySelectorAll("tr");
+        //const cells = rows[target].querySelectorAll("td");
+
+        //find all cells in the correct column
+        const cells = tableBody.querySelectorAll(`tr td:nth-of-type(${targetColumn})`);
+        //console.log(cells);
+
+        //run through each cell in the column to reassign selected cell
+        for(let i = 0; i < cells.length; i++) {
+            //console.log(cells[i].textContent + cells[i].getAttribute("id"));
+
+            if(cells[i].id == "selected") {
+                cells[i].removeAttribute("id");
+                
+                //loop back to start of array if next value is blank
+                if(typeof cells[i + 1] === "undefined" || cells[i + 1].textContent == "") {
+                    cells[0].id = "selected";
+                }
+                //otherwise, set the next value to selected
+                else {
+                    cells[i + 1].id = "selected";
+                }
+
+                //break so that the selected id doesnt loop back around to its original placement
+                break;
+            }
+        }
+	}
+});
