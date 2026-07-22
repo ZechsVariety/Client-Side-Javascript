20478364374f0f39ad3ad1e7ecc06eec626ea54a
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-07-22 19:21:22 -0400

    A2: failed validation attempt
    by retrieving the elements at the start, any changes made to the elements using inspect element will update the element in the array. im gonna redo this and just manually input the available values

--- assignment-2/js/pizza.js
+++ assignment-2/js/pizza.js
@@ -3,12 +3,20 @@ const footerText = document.querySelector("footer p");
 
 const form = document.querySelector('form');
 const submitButton = document.getElementById("submit");
+const errorDisplay = document.getElementById("error");
 
 //variables
 
+//updated when form is submitted
 let size, crust;
 let toppings = [];
 
+//these are set at page load and used for validation. (ie: form submissions must match these, otherwise it's assumed that the person edited the html with inspect element)
+const availableSizes = document.querySelectorAll('input[name="size"]');
+const availableToppings = document.querySelectorAll('input[name="toppings[]"]');
+const availableCrusts = document.querySelectorAll("#crust");
+
+let error;
 
 const creatorName = "Zecheriah Ferguson";
 const creatorStudentID = 200639774;
@@ -17,9 +25,25 @@ const creatorStudentID = 200639774;
 footerText.textContent = `Site by ${creatorName} - ${creatorStudentID}`;
 
 function getFormData() {
+    error = "";
+
     //retrieve the selected size node
     //thank you Parthik Gosar for the code to find the currently selected radio button: https://stackoverflow.com/a/15839451
     size = document.querySelector('input[name="size"]:checked');
+
+    //check if size option exists in the availableSizes array
+    for(let i = 0; i < availableSizes.length; i++) {
+        if(availableSizes[i].value === size.value) {
+            console.log("success");
+            break;
+        }
+
+        if(i == availableSizes.length - 1) {
+            error = "Invalid size.";
+            console.log("invalid size");
+            return;
+        }
+    }
 
     //retrieve all selected topping nodes
     toppings = document.querySelectorAll('input[name="toppings[]"]:checked');
@@ -32,4 +56,10 @@ submitButton.addEventListener('click', function(event) {
     event.preventDefault();
     
     getFormData();
+
+    errorDisplay.textContent = error;
+
+    if(error != null || "") {
+        return;
+    }
 });
