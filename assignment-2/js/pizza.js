581ac98f2106be3c08805f638667f878bd19282f
ZechsVariety <mortimer@promail.ca>
2026-07-22 16:55:22 -0400

    A2: retrieve pizza size, toppings and crust type
    currently this happens as soon as you enter the page, and thus only retrieves the default values, but this can easily be moved to a function later

--- assignment-2/js/pizza.js
+++ assignment-2/js/pizza.js
@@ -1,6 +1,24 @@
 //objects
 const footerText = document.querySelector("footer p");
 
+const form = document.querySelector('form');
+
+//retrieve form data (later this will be moved to a function)
+
+//retrieve the selected size option
+//thank you Parthik Gosar for the code to find the currently selected radio button: https://stackoverflow.com/a/15839451
+let size = document.querySelector('input[name="size"]:checked').value;
+
+//retrieve all selected toppings
+let toppingNodes = document.querySelectorAll('input[name="toppings[]"]:checked');
+//populate a toppings array list with the actual values (as strings)
+let toppings = [];
+for(let i = 0; i < toppingNodes.length; i++) {
+    toppings[i] = toppingNodes[i].value;
+}
+
+let crust = document.getElementById("crust").value;
+
 //variables
 const creatorName = "Zecheriah Ferguson";
 const creatorStudentID = 200639774;
