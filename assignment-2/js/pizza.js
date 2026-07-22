2f61a038ae22b765b536c8ef6cee2274309e20c7
ZechsVariety <mortimer@promail.ca>
2026-07-22 16:33:07 -0400

    A2: dynamically add name and student number to footer

---
+++ assignment-2/js/pizza.js
@@ -0,0 +1,9 @@
+//objects
+const footerText = document.querySelector("footer p");
+
+//variables
+const creatorName = "Zecheriah Ferguson";
+const creatorStudentID = 200639774;
+
+//dynamically add name and student id to footer
+footerText.textContent = `Site by ${creatorName} - ${creatorStudentID}`;
