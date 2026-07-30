6d15b101e9dc295848b9fd75dab41c3b60c86fca
ZechsVariety <mortimer@promail.ca>
2026-07-29 20:10:40 -0400

    L11 done: battery api

--- lesson-11/js/battery.js
+++ lesson-11/js/battery.js
@@ -12,20 +12,31 @@ const chargeMeter = document.querySelector(
 /* Functions
 -------------------------------------------------- */
 // STEP 3a: Create the updateBatteryStatus() function
+function updateBatteryStatus(battery) {
+    // STEP 3b: Update the charging status
+    if(battery.charging) {
+        chargeStatus.textContent = "charging...";
+    } else {
+        chargeStatus.textContent = "unplugged";
+    }
 
-// STEP 3b: Update the charging status
-
-// STEP 3c: Update the charge level
+    // STEP 3c: Update the charge level
+    chargeLevel.textContent = battery.level * 100 + "%";
+    chargeMeter.value = battery.level * 100;
+}
 
 // STEP 2a: Using the getBattery() method of the navigator object,
 //create a promise to retrieve the battery information
+navigator.getBattery().then((battery) => {
+    // STEP 2b: See what the battery object contains
+    console.log(battery);
 
-// STEP 2b: See what the battery object contains
+    // STEP 3d: Update the battery information when the promise resolves
+    updateBatteryStatus(battery);
 
-// STEP 3d: Update the battery information when the promise resolves
+    // STEP 4a: Event listener for changes to the charging status
 
-// STEP 4a: Event listener for changes to the charging status
-
-// STEP 4b: Event listener for changes to the charge level
+    // STEP 4b: Event listener for changes to the charge level
+})
 
 /* This script adapted from the excellent code examples found at https://www.w3.org/TR/battery-status/#examples and https://developer.mozilla.org/en-US/docs/Web/API/Battery_Status_API */
