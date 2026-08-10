c069471b593d8686a40ea8c6a13666ff2e43b2dd
ZechsVariety <mortimer@promail.ca>
2026-08-10 16:33:40 -0400

    A4: fetch videos from youtube playlist and display their titles
    switched to youtube api because I found out spotify's api requires a premium subscription

--- assignment-3/js/index.js
+++ assignment-3/js/index.js
@@ -1,5 +1,6 @@
 //object references
 const footerText = document.querySelector("footer p");
+const videoSection = document.querySelector("section");
 
 //footer data
 const creatorName = "Zecheriah Ferguson";
@@ -7,3 +8,15 @@ const creatorStudentID = 200639774;
 
 //dynamically add name and student id to footer
 footerText.textContent = `Demonstration by: ${creatorName} - ${creatorStudentID}`;
+
+//this url was generated using https://developers.google.com/youtube/v3/docs/playlistItems/list
+fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=PLOSODkKpkvaQQd0AFDbe2vNvfRhhLAz6a&key=AIzaSyCSOE1xMZVew7RUii8E24JSRfXLQi4C9Gs")
+    .then(result => result.json())
+    .then(json => {
+        //run through each video that was retrieved
+        json.items.forEach(video => {
+            //add video title as an h3 element
+            videoSection.innerHTML += `<h3>${video.snippet.title}</h3>`;
+        });
+    }
+);
