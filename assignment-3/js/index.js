ffab1ba82c086dd6cc1c8d2d60bca81791e86bdd
ZechsVariety <mortimer@promail.ca>
2026-08-10 16:46:38 -0400

    A3: verify response is okay before getting json
    btw i accidentally called the previous commits A4

--- assignment-3/js/index.js
+++ assignment-3/js/index.js
@@ -10,9 +10,23 @@ const creatorStudentID = 200639774;
 footerText.textContent = `Demonstration by: ${creatorName} - ${creatorStudentID}`;
 
 //this url was generated using https://developers.google.com/youtube/v3/docs/playlistItems/list
+//TODO: change max results to be a variable rather than a hardcoded 15
 fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=PLOSODkKpkvaQQd0AFDbe2vNvfRhhLAz6a&key=AIzaSyCSOE1xMZVew7RUii8E24JSRfXLQi4C9Gs")
-    .then(result => result.json())
+    //get response
+    .then(response => {
+        console.log(response);
+
+        //true/false if response is good
+        if(!response.ok) {
+            throw new Error("ERROR: Could not fetch videos!");
+        }
+
+        return response.json();
+    })
+    //if response is okay, get and use json
     .then(json => {
+        console.log(json);
+
         //run through each video that was retrieved
         json.items.forEach(video => {
             //add video title as an h3 element
